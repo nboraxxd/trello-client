16fc39ec2ec9be9f75a3364c77e0dbcb0e5d8673
nboraxxd <nhatbaoxxd@gmail.com>
2024-10-16 19:36:18 +0700

    feat: Add BoardBar component

--- src/components/board-bar/board-bar.tsx
+++ src/components/board-bar/board-bar.tsx
@@ -1,17 +1,109 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+import { BOARD_BAR_LEFT_MENU } from '@/constants/list'
+
+const MENU_STYLES = {
+  px: '8px',
+  border: 'none',
+  borderRadius: '4px',
+  bgcolor: 'transparent',
+  color: 'white',
+  '&:hover': {
+    bgcolor: 'primary.50',
+  },
+  '& .MuiSvgIcon-root': {
+    ml: 0,
+    color: 'white',
+  },
+  '& .MuiChip-label': {
+    pr: 0,
+  },
+}
 
 export default function BoardBar() {
   return (
     <Box
       sx={{
-        height: (theme) => theme.trello.boardBarHeight,
-        width: '100%',
-        backgroundColor: 'primary.dark',
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        backgroundColor: 'primary.dark',
+        px: 2,
+        overflowX: 'auto',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
       }}
     >
-      Board Bar
+      {/* Left */}
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        {BOARD_BAR_LEFT_MENU.map(({ label, icon: IconComp }) => (
+          <Chip key={label} icon={<IconComp />} label={label} clickable sx={MENU_STYLES} />
+        ))}
+      </Box>
+
+      {/* Right */}
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button
+          variant="outlined"
+          sx={{
+            color: 'white',
+            borderColor: 'white',
+            ':hover': {
+              borderColor: 'grey.300',
+            },
+          }}
+          startIcon={<PersonAddIcon />}
+        >
+          Invite
+        </Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0be' },
+            },
+          }}
+        >
+          <Tooltip title="Trung Quan Dev">
+            <Avatar
+              alt="Trung Quan Dev"
+              src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg"
+            />
+          </Tooltip>
+          <Tooltip title="Helena">
+            <Avatar alt="Helena" src="https://randomuser.me/api/portraits/women/12.jpg" />
+          </Tooltip>
+          <Tooltip title="Velma">
+            <Avatar alt="Velma" src="https://randomuser.me/api/portraits/women/54.jpg" />
+          </Tooltip>
+          <Tooltip title="Aoife">
+            <Avatar alt="Aoife" src="https://randomuser.me/api/portraits/women/34.jpg" />
+          </Tooltip>
+          <Tooltip title="Lucie">
+            <Avatar alt="Lucie" src="https://randomuser.me/api/portraits/women/40.jpg" />
+          </Tooltip>
+          <Tooltip title="Harris">
+            <Avatar alt="Harris" src="https://randomuser.me/api/portraits/women/55.jpg" />
+          </Tooltip>
+          <Tooltip title="Angie">
+            <Avatar alt="Angie" src="https://randomuser.me/api/portraits/women/21.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
--- src/constants/list.ts
+++ src/constants/list.ts
@@ -1,6 +1,11 @@
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
+import BoltIcon from '@mui/icons-material/Bolt'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import FilterListIcon from '@mui/icons-material/FilterList'
 
 export const THEMES = [
   {
@@ -17,5 +22,28 @@ export const THEMES = [
     label: 'System',
     value: 'system',
     icon: SettingsBrightnessOutlinedIcon,
+  },
+] as const
+
+export const BOARD_BAR_LEFT_MENU = [
+  {
+    label: 'TrungQuanDev MERN Stack Board',
+    icon: DashboardIcon,
+  },
+  {
+    label: 'Public/Private Workspace',
+    icon: VpnLockIcon,
+  },
+  {
+    label: 'Add To Google Drive',
+    icon: AddToDriveIcon,
+  },
+  {
+    label: 'Automation',
+    icon: BoltIcon,
+  },
+  {
+    label: 'Filter',
+    icon: FilterListIcon,
   },
 ] as const
